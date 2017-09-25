f5982664b9d94ac5e7f353640aed45d6263fd4ed
Pedro E. Thormodsen <pedro@thormodsen.com>
2017-09-26 01:16:03 +0200

    changed the help message

--- bot/messenger.py
+++ bot/messenger.py
@@ -21,7 +21,7 @@ class Messenger(object):
     def write_help_message(self, channel_id):
         bot_uid = self.clients.bot_user_id()
         txt = '{}\n{}\n{}\n{}'.format(
-            "I'm your friendly Slack bot written in Python.  I'll *_respond_* to the following commands:",
+            "Hello. I'm the first incarnation of a brilliant idea coach. For now, I'll *_respond_* to the following commands:",
             "> `hi <@" + bot_uid + ">` - I'll respond with a randomized greeting mentioning your user. :wave:",
             "> `<@" + bot_uid + "> joke` - I'll tell you one of my finest jokes, with a typing pause for effect. :laughing:",
             "> `<@" + bot_uid + "> attachment` - I'll demo a post with an attachment using the Web API. :paperclip:")
